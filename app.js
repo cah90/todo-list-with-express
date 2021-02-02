12ac7792599350c05cdcb34161cfd0f59110572f
Cássia <cascrisbern@gmail.com>
2021-02-02 17:16:19 -0300

    rendering all tasks in a simple way

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const tasks = require("./tasks.json")
+
 const express = require("express")
 const app = express()
 const port = 3000
@@ -5,6 +7,19 @@ const port = 3000
 
 app.get('/', (req,res) => {
   res.send("Hello World!")
+})
+
+app.get('/tasks', (req,res) => {
+  
+  let html = '<html><head><title>All Tasks</title></head><body><ul>'
+  
+  for (task of tasks.allTasks) {
+    html += `<li>${task.title}</li>`
+  }
+  
+  html += '</ul></body></html>'
+  
+  res.send(html)
 })
 
 
